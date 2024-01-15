e468672ded5666af93cf19ee00364a455ddebc1c
Cppshizoid <alex.krivoschecov@gmail.com>
2024-01-16 06:39:54 +0700

    Create constant.hpp

---
+++ include/constant.hpp
@@ -0,0 +1,22 @@
+#ifndef CONSTANT_HPP
+#define CONSTANT_HPP
+
+#include <cstddef>
+
+namespace web_server {
+
+constexpr unsigned int SOCKET_LISTEN_QUEUE_SIZE = 512;
+
+constexpr unsigned int MAX_BUFFER_RING_SIZE = 65536;
+
+constexpr size_t IO_URING_QUEUE_SIZE = 2048;
+
+constexpr unsigned int BUFFER_GROUP_ID = 0;
+
+constexpr unsigned int BUFFER_RING_SIZE = 4096;
+
+constexpr size_t BUFFER_SIZE = 1024;
+
+} // namespace web_server
+
+#endif
